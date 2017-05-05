488a5595d99b7a81a0cab5d5808338dab938605e
Haxman2 <tonyenns12@hotmail.com>
2017-05-05 16:30:17 -0600

    Added default aabb to block register

--- src/org/platformer/register/RegisterBlocks.java
+++ src/org/platformer/register/RegisterBlocks.java
@@ -3,12 +3,17 @@ package org.platformer.register;
 import java.util.HashMap;
 
 import org.platformer.block.Block;
+import org.platformer.utils.AABB;
 
 public class RegisterBlocks
 {
+	private static final AABB DEFAULT_AABB = new AABB(16f,16f);
 	public static HashMap<Integer,Block> blocks = new HashMap<Integer,Block>();
-	public static Block dirt = new Block(0,"dirt");
-	public static Block stone = new Block(1,"stone");
+	
+	/** Block Parameters: BlockID, textureName*/
+	
+	public static Block dirt = new Block(0,"dirt").setCollision(DEFAULT_AABB);
+	public static Block stone = new Block(1,"stone").setCollision(DEFAULT_AABB);
 
 	public static void init()
 	{
@@ -16,13 +21,22 @@ public class RegisterBlocks
 		register(stone);
 	}
 
+	/**
+	 * Add new block to registry
+	 * @param block
+	 */
 	private static void register(Block block)
 	{
 		blocks.put(block.getID(), block);
 	}
 	
-	public static Block get(int i)
+	/**
+	 * Get block from registry using its block ID
+	 * @param id - Block ID
+	 * @return Block
+	 */
+	public static Block get(int id)
 	{
-		return blocks.get(i);
+		return blocks.get(id);
 	}
 }
