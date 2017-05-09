d5d78baada5d122e316bba3aa675855bd6ca5298
michael <michaellt@hotmail.ca>
2017-05-08 22:37:22 -0400

    Added PlayerConfiguration

--- src/org/platformer/entity/EntityPlayerLocal.java
+++ src/org/platformer/entity/EntityPlayerLocal.java
@@ -1,5 +1,6 @@
 package org.platformer.entity;
 
+import org.platformer.Data.PlayerConfiguration;
 import org.platformer.register.RegisterKeybinds;
 import org.platformer.utils.MiscUtils;
 import org.platformer.world.World;
@@ -13,17 +14,31 @@ public class EntityPlayerLocal extends EntityPlayer
 		super(world, hash);
 	}
 
+	/**
+	 * Constructor with PlayerConfiguration added.
+	 *
+	 * @param world
+	 * @param hash
+	 * @param config
+	 */
+	public EntityPlayerLocal(World world, String hash, PlayerConfiguration config)
+	{
+
+		super(world, hash, config);
+
+	}
+
 	@Override
 	public void update()
 	{
 		if(RegisterKeybinds.move_left.isPressed()) motionX = -4f;
 		if(RegisterKeybinds.move_right.isPressed()) motionX = 4f;
-		
+
 		if(onGround)
 		{
 			if(RegisterKeybinds.move_up.isPressed()) motionY = -4.2f;
 		}
-		
+
 		mouseAngle = MiscUtils.pointTowardsPosition(posX, posY-20, ((WorldClient)world).getMouseInWorld()[0], ((WorldClient)world).getMouseInWorld()[1]);
 		super.update();
 	}
