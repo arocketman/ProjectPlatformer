a9027d9d7a2efdfefa654e241545718463cc0715
Michael Leblanc <michaellt@hotmail.ca>
2017-05-09 01:28:49 -0400

    Removed useless functions, edited init()
    I just realized I could have added RenderItem in the init function rather than by adding a new function

--- src/org/platformer/register/RegisterRenders.java
+++ src/org/platformer/register/RegisterRenders.java
@@ -15,34 +15,9 @@ public class RegisterRenders
 
 	public static void init()
 	{
+
 		entityRenderMap.put(EntityPlayerLocal.class, new RenderPlayer());
-	}
-
-	/**
-	 * Adds an entity to the entityRenderMap
-	 *
-	 * @param ent entity to be added
-	 */
-	public static void addRenderEntity(Entity ent) {
-
-		if(ent instanceof EntityItem) {
-
-			EntityItem entItem = (EntityItem)ent;
-			entityRenderMap.put(ent.getClass(), new RenderItem());
-
-		}
-
-	}
-
-	/**
-	 * Removes an entity from the entityRenderMap
-	 *
-	 * @param ent entity to be removed
-	 */
-	public static void removeRenderEntity(Entity ent)
-	{
-
-		entityRenderMap.remove(ent.getClass());
+		entityRenderMap.put(EntityItem.class, new RenderItem());
 
 	}
 
